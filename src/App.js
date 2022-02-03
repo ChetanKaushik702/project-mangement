341adc8927c196278951192e3103e193f296e4e6
Chetan Kaushik <kaushikchetan199@gmail.com>
2022-02-03 11:04:08 +0530

    Created navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router} from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router> 
+      <div className='App'>
+        <Navbar />
+      </div>
+    </Router>
   );
 }
 
